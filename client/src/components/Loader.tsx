ad085bfde02e418420f27389ba35370ab32e2bfb
alexspdlr <42113852+alexspdlr@users.noreply.github.com>
2022-06-18 03:29:22 +0200

    💄 Animate size transitions of alternative popover

--- client/src/components/Loader.tsx
+++ client/src/components/Loader.tsx
@@ -4,16 +4,9 @@ import Box from '@mui/material/Box';
 
 export default function Loader() {
   return (
-    <Box
-      sx={{
-        display: 'flex',
-        alignItems: 'center',
-        justifyContent: 'center',
-        width: 200,
-        height: 50,
-      }}
-    >
-      <CircularProgress size='1.2rem' sx={{ color: 'rgba(51, 51, 51, 0.3)' }} />
-    </Box>
+    <div style={{ backgroundColor: 'red' }}>
+      test
+      {/* <CircularProgress size='1.3em' sx={{ color: 'rgba(51, 51, 51, 0.3)' }} />*/}
+    </div>
   );
 }
--- client/src/components/Popover.tsx
+++ client/src/components/Popover.tsx
@@ -1,34 +1,49 @@
 import React, { ReactNode } from 'react';
 import styled from '@emotion/styled';
-import { Popover, Fade } from '@mui/material';
+import { Popover, Fade, Divider, Collapse } from '@mui/material';
 import Loader from './Loader';
+import AnimateHeight from './AnimateHeight';
+import { CSSTransition, TransitionGroup } from 'react-transition-group';
 
 const PopoverContainer = styled('div')(
-  (props) => ` 
+  (props) => `  
   -webkit-transition: all 0.25s ease-out;
   -moz-transition: all 0.25s ease-out;
   -o-transition: all 0.25s ease-out;
   transition: all 0.25s ease-out; 
-  font-size: 22px; 
-  font-weight: 400;  
-  color: #333;
-  height: auto; 
+    font-size: 22px; 
+  font-weight: 400;   
     `
 );
 
+const StyledCSSTransition = styled(CSSTransition)(
+  () => `
+  -webkit-transition: all 0.5s ;
+  -moz-transition: all 0.5s ;
+  -o-transition: all 0.5s ;
+  transition: all 0.5s ; 
+  `
+);
+
+interface AlternativeProp {
+  disabled?: boolean;
+}
 const Alternative = styled('div')(
-  (props) => ` 
-  padding: 6px 11px 6px 11px; 
-  font-weight: 300; 
-  cursor: pointer; 
-  -webkit-transition: all 0.25s ease-out;
+  (props: AlternativeProp) => ` 
+    -webkit-transition: all 0.25s ease-out; 
   -moz-transition: all 0.25s ease-out;
   -o-transition: all 0.25s ease-out;
-  transition: all 0.25s ease-out; 
-  border-bottom: 1px solid rgba(0, 0, 0, 0.05);
-  &:hover {
+  transition: all 0.25s ease-out;  
+  padding: 6px 11px 6px 11px; 
+  font-weight: 300; 
+  border-bottom: 1px solid rgba(0, 0, 0, 0.05); 
+  ${!props.disabled && `cursor: pointer;`} 
+  ${
+    !props.disabled &&
+    `&:hover {
     background-color: rgba(0, 99, 149, 0.2); 
-  }
+  }`
+  } 
     `
 );
 
@@ -50,7 +65,7 @@ const CustomPopover = ({
       open={open}
       anchorEl={anchorEl}
       onClose={onClose}
-      transitionDuration={{ enter: 400, exit: 250 }}
+      transitionDuration={0}
       anchorOrigin={{
         vertical: 'bottom',
         horizontal: 'left',
@@ -76,19 +91,26 @@ const CustomPopover = ({
           boxShadow: '0 1px 4px 0 rgb(0 0 0 / 10%)',
           border: '1px solid #dae1e8',
           maxHeight: '250px',
+          width: 'auto',
+          height: 'auto',
         },
       }}
     >
       <PopoverContainer>
-        {alternatives.length > 0 ? (
-          <div>
-            {alternatives.map((alternative) => (
-              <Alternative>{alternative}</Alternative>
-            ))}
+        <AnimateHeight>
+          {' '}
+          <div style={{ position: 'relative' }}>
+            {alternatives.length > 0 ? (
+              <>
+                {alternatives.map((alternative, i) => (
+                  <Alternative>{alternative}</Alternative>
+                ))}
+              </>
+            ) : (
+              <Alternative disabled>...</Alternative>
+            )}
           </div>
-        ) : (
-          <Loader />
-        )}
+        </AnimateHeight>
       </PopoverContainer>
     </Popover>
   );
