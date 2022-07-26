07fa639e91903014d5aa681e089db5ddc8145f1e
alexspdlr <42113852+alexspdlr@users.noreply.github.com>
2022-07-26 22:35:47 +0200

    Improve SourceSelect sentence style

--- client/src/components/rephrase/subcomponents/source/subcomponents/SourceSelect.tsx
+++ client/src/components/rephrase/subcomponents/source/subcomponents/SourceSelect.tsx
@@ -1,5 +1,5 @@
 import styled from '@emotion/styled';
-import React, { useEffect, useRef } from 'react';
+import React, { useEffect, useRef, useState } from 'react';
 import useRephraseToolTextboxHeight from 'src/utils/hooks/useRephraseToolTextboxHeight';
 import splitIntoSentences from 'src/utils/splitIntoSentences';
 
@@ -23,17 +23,35 @@ const Container = styled('div')(
       `
 );
 
+interface SentenceProps {
+  hovered: boolean;
+  otherTokenHovered: boolean;
+}
+
 const Sentence = styled('span')(
-  () => `
+  (props: SentenceProps) => `
   padding-top: 2px; 
   padding-bottom: 2px; 
   border-top: 1px solid #fff;
   border-bottom: 1px solid #fff;
   white-space: pre-wrap;
-  &:hover {
-    background-color: rgba(0, 99, 149, 0.15);
+
+  ${
+    props.hovered
+      ? `
+    background-color: #EDF1F3; 
+    color: #0F2B46;
     cursor: pointer; 
-    transition: background-color 0.15s ease-out; 
+    transition: 0.2s background-color, 0.2s color;
+    `
+      : props.otherTokenHovered
+      ? `
+    color: #bbbdc0;
+    transition: 0.2s color;
+    `
+      : `
+      color: #333333;
+      transition: 0.2s color;`
   }
   `
 );
@@ -46,6 +64,7 @@ const SourceSelect = (props: SourceSelectProps) => {
   const { value } = props;
   const containerRef = useRef(null);
   useRephraseToolTextboxHeight(value, containerRef);
+  const [hoveredSentence, setHoveredSentence] = useState<string | null>(null);
 
   useEffect(() => {
     document.getElementById('source-select-container')?.focus();
@@ -55,9 +74,19 @@ const SourceSelect = (props: SourceSelectProps) => {
     <Container ref={containerRef} tabIndex={0} id='source-select-container'>
       {splitIntoSentences(value).map((token, i) =>
         token.kind === 'sentence' ? (
-          <Sentence key={`token_${i}`}>{token.value}</Sentence>
+          <Sentence
+            onMouseEnter={() => setHoveredSentence(`token_${i}`)}
+            onMouseLeave={() => setHoveredSentence(null)}
+            hovered={hoveredSentence === `token_${i}`}
+            otherTokenHovered={
+              hoveredSentence !== null && hoveredSentence !== `token_${i}`
+            }
+            key={`token_${i}`}
+          >
+            {token.value}
+          </Sentence>
         ) : (
-          <span key={`token_${i}`} style={{ whiteSpace: 'pre' }}>
+          <span key={`token_${i}`} style={{ whiteSpace: 'pre-wrap' }}>
             {token.value}
           </span>
         )
