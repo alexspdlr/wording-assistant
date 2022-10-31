087b4c4cd277cd12e12e0b0884ab58fa31d7329a
alexspdlr <42113852+alexspdlr@users.noreply.github.com>
2022-10-31 16:00:56 +0100

    🚀 Checkpoint: Puppeteer launches in Docker compose network

--- server/src/operations/setup.ts
+++ server/src/operations/setup.ts
@@ -2,10 +2,20 @@ import puppeteer from 'puppeteer';
 
 const setup = async () => {
   /* Load broser & DeepL Page */
+  console.log('puppeteer: _______', JSON.stringify(puppeteer));
   const browser = await puppeteer.launch({
     headless: true,
-    args: ['--no-sandbox', '--disabled-setupid-sandbox'],
+    dumpio: true,
+    ignoreHTTPSErrors: true,
+    args: [
+      '--no-sandbox',
+      '--disable-dev-shm-usage',
+      '--devtools-flags=disable',
+    ],
   });
+
+  console.log('AFTER BRWOSER LAUNCH');
+
   const page = await browser.newPage();
   page.setDefaultTimeout(8000);
   await page.goto('https://www.deepl.com/en/translator#en/de/');
