21ed9e58e29eefa7c867814cff1d2a04cb69bbf0
alexspdlr <42113852+alexspdlr@users.noreply.github.com>
2022-11-17 10:48:25 +0100

    💄 Remove unnecessary "selected i/j" info

--- client/src/components/rephrase/subcomponents/target/subcomponents/TargetActions.tsx
+++ client/src/components/rephrase/subcomponents/target/subcomponents/TargetActions.tsx
@@ -3,7 +3,6 @@ import { ReactComponent as CheckIcon } from 'src/assets/check.svg';
 import { ReactComponent as ClearIcon } from 'src/assets/ClearIcon.svg';
 import IconButton from 'src/components/general/icon-button';
 import Tooltip from 'src/components/general/tooltip';
-import { MAX_SOURCE_SELECTION_LENGTH } from 'src/constants';
 import useBoundStore from 'src/store';
 
 /* ---------------------------- Styled components --------------------------- */
@@ -40,17 +39,12 @@ interface TargetActionsProps {
 
 const TargetActions = (props: TargetActionsProps) => {
   const { resetToOriginalSelection } = props;
-  const originalTextLength = useBoundStore(
-    (state) => state.uiState.originalTextSelection?.value.length
-  );
   const deselectText = useBoundStore((state) => state.deselectText);
 
   return (
     <Layout>
-      <Fragment>
-        {originalTextLength &&
-          `Selected: ${originalTextLength} / ${MAX_SOURCE_SELECTION_LENGTH}`}
-      </Fragment>
+      <Fragment />
+
       <Fragment>
         <Tooltip content='Decline' direction='top'>
           <IconButton
